17ddada429c370295b9964d1d13330d07607ce1c
Robert Boehme <r.boehme@outlook.com>
2019-03-10 20:11:55 -0500

    Set up date time conversion

--- src/main/java/com/example/EmpData/EmpDataApplication.java
+++ src/main/java/com/example/EmpData/EmpDataApplication.java
@@ -2,12 +2,25 @@ package com.example.EmpData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		EmpDataApplication.class,
+		Jsr310JpaConverters.class
+})
 public class EmpDataApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpDataApplication.class, args);
 	}
-
 }

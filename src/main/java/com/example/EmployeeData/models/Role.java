b30bd528a5cfb3185568eb8023a4369df5e7760a
Robert Boehme <r.boehme@outlook.com>
2019-03-11 01:34:35 -0500

    Added database connection settings and started building user and role classes.

---
+++ src/main/java/com/example/EmployeeData/models/Role.java
@@ -0,0 +1,4 @@
+package com.example.EmployeeData.models;
+
+public class Role {
+}
---
+++ src/main/java/com/example/EmployeeData/models/RoleType.java
@@ -0,0 +1,6 @@
+package com.example.EmployeeData.models;
+
+public enum RoleType {
+    ROLE_MANAGER,
+    ROLE_EMPLOYEE
+}
